bf0cf3f8b73514e46b8d30fd9f248b08b1f57066
Kayque Ferreira <kayque.ferreira60@aluno.ifce.edu.br>
2024-10-28 17:21:53 -0300

    Updating app removing 'select project'

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
-import SideSelector from "./components/SideSelector/SideSelector";
 import Professional from "./projects/professional/app/Professional";
 
 function App() {
-  const [selectedSide, setSelectedSide] = useState("none");
-  console.log(selectedSide);
+  // const [selectedSide, setSelectedSide] = useState("none");
+  // console.log(selectedSide);
 
   const handleRender = () => (
     <div className="app">
-      <SideSelector
-        selectedSide={selectedSide}
-        setSelectedSide={setSelectedSide}
-      />
-
-      {selectedSide == "Professional" && <Professional />}
+      <Professional />
     </div>
   );
 
